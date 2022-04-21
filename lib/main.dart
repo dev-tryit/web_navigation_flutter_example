5cefc344ed46456c317703b562d310d6ba4c6691
donghyun kim <imkim1893@naver.com>
2022-04-21 17:48:24 +0900

    Navigator 완성

--- lib/main.dart
+++ lib/main.dart
@@ -12,48 +12,53 @@ void main() {
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
   final GoRouter _router = GoRouter(
+    //이동 애니메이션 수정 참고
+    //https://github.com/csells/go_router/blob/master/example/lib/transitions.dart
     initialLocation: '${MainPage.pageName}',
     routes: <GoRoute>[
       GoRoute(
         path: '${MainPage.pageName}',
-        builder: (BuildContext context, GoRouterState state) =>MainPage(),
+        pageBuilder: (context, state) =>
+            NoTransitionPage<void>(child: MainPage()),
       ),
       GoRoute(
         path: '${Sub1Page.pageName}',
-        builder: (BuildContext context, GoRouterState state) => Sub1Page(),
+        pageBuilder: (context, state) =>
+            NoTransitionPage<void>(child: Sub1Page()),
       ),
       GoRoute(
         path: '${Sub2Page.pageName}',
-        builder: (BuildContext context, GoRouterState state) => Sub2Page(),
+        pageBuilder: (context, state) =>
+            NoTransitionPage<void>(child: Sub2Page()),
       ),
     ],
   );
 
   @override
   Widget build(BuildContext context) => MaterialApp.router(
-    routeInformationParser: _router.routeInformationParser,
-    routerDelegate: _router.routerDelegate,
-    title: 'GoRouter Example',
-  );
-  // // This widget is the root of your application.
-  // @override
-  // Widget build(BuildContext context) {
-  //   return MaterialApp(
-  //     title: '웹페이지입니다',
-  //     home: MainPage(),
-  //     initialRoute: "/${MainPage.pageName}",
-  //     onGenerateRoute: (settings) {
-  //       if (settings.name == "/${Sub1Page.pageName}") {
-  //         return PageRouteBuilder(
-  //           settings: settings,
-  //           pageBuilder: (_, __, ___) => Sub1Page(),
-  //         );
-  //       }
-  //       return PageRouteBuilder(
-  //         settings: settings,
-  //         pageBuilder: (_, __, ___) => MainPage(),
-  //       );
-  //     },
-  //   );
-  // }
+        routeInformationParser: _router.routeInformationParser,
+        routerDelegate: _router.routerDelegate,
+        title: 'GoRouter Example',
+      );
+// // This widget is the root of your application.
+// @override
+// Widget build(BuildContext context) {
+//   return MaterialApp(
+//     title: '웹페이지입니다',
+//     home: MainPage(),
+//     initialRoute: "/${MainPage.pageName}",
+//     onGenerateRoute: (settings) {
+//       if (settings.name == "/${Sub1Page.pageName}") {
+//         return PageRouteBuilder(
+//           settings: settings,
+//           pageBuilder: (_, __, ___) => Sub1Page(),
+//         );
+//       }
+//       return PageRouteBuilder(
+//         settings: settings,
+//         pageBuilder: (_, __, ___) => MainPage(),
+//       );
+//     },
+//   );
+// }
 }
